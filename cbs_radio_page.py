56acef1cf853004b41b1a3307cf91db6b03e3271
byskull <byskull.kim@gmail.com>
2020-04-22 11:47:43 +0900

    pageadd3
    pageadd3

--- cbs_radio_page.py
+++ cbs_radio_page.py
@@ -57,39 +57,3 @@ for td in tds :
             print "End of Page"
             pass
         
-
-    #br.open("https://www.cbs.co.kr/radio/pgm/board.asp?pn=list&skey=&sval=&bgrp=2&page=&bcd=00350012&pgm=111&mcd=BOARD2" )
-    #        "https://www.cbs.co.kr/radio/pgm/board.asp?pn=read&skey=&sval=&anum=519020&vnum=" "&bgrp=2&page=&bcd=00350012&pgm=111&mcd=BOARD2"
-    
-'''
-bd_menu_content
-
-https://www.cbs.co.kr/radio/pgm/board.asp?pn=list&skey=&sval=&bgrp=2&page=&bcd=00350012&pgm=111&mcd=BOARD2
-
-https://www.cbs.co.kr/radio/pgm/board.asp?pn=read&skey=&sval=&anum=519020&vnum=6891&bgrp=2&page=&bcd=00350012&pgm=111&mcd=BOARD2
-
-https://www.cbs.co.kr/radio/pgm/board.asp?pn=read&skey=&sval=&anum=519013&vnum=6890&bgrp=2&page=&bcd=00350012&pgm=111&mcd=BOARD2
-
-
-<form id="frmBoardList" name="frmBoardList" method="post" action="">
-	<table border="0" cellspacing="0" cellpadding="0" width="100%">
-		<tr class="bd_menu" style="height:25px" align="center">
-
-			<td style="white-space:nowrap;width:45px" class="bd_menu">번호</td>
-
-			<td style="white-space:nowrap;width:30px" class="bd_menu">첨부</td>
-
-			<td style="white-space:nowrap" class="bd_menu">제목</td>
-
-			<td style="white-space:nowrap;width:100px" class="bd_menu">글쓴이</td>
-
-			<td style="white-space:nowrap;width:30px" class="bd_menu">조회</td>
-
-			<td style="white-space:nowrap;width:70px" class="bd_menu">날짜</td>
-
-		</tr>
-
-		<tr class="bd_list" style="height:20px" align="center">
-
-			<td style="white-space:nowrap" class="bd_list"><strong>6891</strong></td>
-'''
